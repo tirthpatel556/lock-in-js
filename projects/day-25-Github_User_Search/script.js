26c472f4ddf74fe15dc7c9837727c0831d3802df
tirthpatel556 <pateltirth7654@gmail.com>
2026-03-07 18:01:39 +0530

    added repoList and improved ui

--- projects/day-25-Github_User_Search/script.js
+++ projects/day-25-Github_User_Search/script.js
@@ -3,15 +3,25 @@ let searchbutton=document.getElementById("searchBtn");
 let profileDiv=document.getElementById("profile");
 
 searchbutton.addEventListener("click",
-    ()=>{
-        let username=usernameInput.value;
-        getProfile(username);
-    }
+ (event)=>{   
+            let username=usernameInput.value;
+            getProfile(username);
+        }
 );
+
+usernameInput.addEventListener("keydown",
+    function(event){
+        if (event.key==="Enter") {
+            let username=usernameInput.value;
+            getProfile(username);
+        }
+    }
+)
 
 //api-call
 
 async function getProfile(username) {
+    profileDiv.innerHTML="<p>Loading Profile...</p>";
     let response= await fetch(`https://api.github.com/users/${username}`);
     let data = await response.json();
     if (!data.login) {
@@ -23,11 +33,21 @@ async function getProfile(username) {
 
 //profile-data
 
-function displayProfile(user){
+async function displayProfile(user){
+    let repos= await getRepos(user.login);
+    let repoList= repos.slice(0,5).map(repo=>`<li>${repo.name}</li>`).join("");
     profileDiv.innerHTML=`
     <img src="${user.avatar_url}" width="100">
     <h3>${user.name}</h3>
     <p>Followers:${user.followers}</p>
     <p>Public repos:${user.public_repos}</P>
+    <h3>Repositories</h3>
+    <ul>${repoList}</ul>
     `;
 };
+
+async function getRepos(username) {
+    let response= await fetch(`https://api.github.com/users/${username}/repos`);
+    let repos= await response.json();
+    return repos;
+}
