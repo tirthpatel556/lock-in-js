4f8c3930ad7259a1c6c0a50b0d59d9dcfc8514f5
tirthpatel556 <pateltirth7654@gmail.com>
2026-03-16 22:40:02 +0530

    improved ui

--- projects/day-31-Quote_Generator_App/script.js
+++ projects/day-31-Quote_Generator_App/script.js
@@ -2,23 +2,40 @@ let quoteText=document.getElementById("quote");
 let authorText=document.getElementById("author");
 let newQuoteBtn=document.getElementById("newQuoteBtn");
 let copyBtn=document.getElementById("copyBtn");
+let quoteBox=document.getElementById("quoteBox");
 
 newQuoteBtn.addEventListener("click",
     getQuote);
 
+document.addEventListener("keydown",
+    function(event){
+        if (event.key==="Enter") {
+            getQuote();
+        }
+    }
+);
+
 async function getQuote() {
-    quoteText.innerText="Genetaring the Quote..."
-    authorText.innerText="";
-    let response= await fetch("https://dummyjson.com/quotes/random");
-    let data= await response.json();
-    displayQuote(data);
-    console.log(data);
+
+    quoteBox.style.opacity="0";
+
+    setTimeout(async function getQuote() {
+
+        quoteText.innerText="Genetaring the Quote..."
+        authorText.innerText="";
+        let response= await fetch("https://dummyjson.com/quotes/random");
+        let data= await response.json();
+        displayQuote(data);
+        quoteBox.style.opacity="2";
+    }, 500);
+    // console.log(data);
 }
 
 function displayQuote(data) {
     quoteText.innerText=data.quote;
     authorText.innerText=data.author;
 };
+getQuote();
 
 copyBtn.addEventListener("click",
     function () {
