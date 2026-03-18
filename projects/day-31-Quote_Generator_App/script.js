395834588c959abbf78a6b054b7a37404db78646
tirthpatel556 <pateltirth7654@gmail.com>
2026-03-18 14:03:09 +0530

    improved style css

--- projects/day-31-Quote_Generator_App/script.js
+++ projects/day-31-Quote_Generator_App/script.js
@@ -2,18 +2,33 @@ let quoteText=document.getElementById("quote");
 let authorText=document.getElementById("author");
 let newQuoteBtn=document.getElementById("newQuoteBtn");
 let copyBtn=document.getElementById("copyBtn");
+// let tweetBtn=document.getElementById("tweetBtn");
 let quoteBox=document.getElementById("quoteBox");
+
+// new quote butoon
 
 newQuoteBtn.addEventListener("click",
     getQuote);
 
-document.addEventListener("keydown",
-    function(event){
-        if (event.key==="Enter") {
-            getQuote();
+    document.addEventListener("keydown",
+        function(event){
+            if (event.key==="Enter") {
+                getQuote();
+            }
         }
-    }
-);
+    );
+    
+// copy button
+        
+    copyBtn.addEventListener("click",
+        function () {
+
+                navigator.clipboard.writeText(quoteText.innerText);
+                copyBtn.innerText="copied";
+        }
+    );
+        
+// quote api
 
 async function getQuote() {
 
@@ -31,15 +46,21 @@ async function getQuote() {
     // console.log(data);
 }
 
+// quote display function
+
 function displayQuote(data) {
-    quoteText.innerText=data.quote;
-    authorText.innerText=data.author;
+    quoteText.innerText=`"${data.quote}"`;
+    authorText.innerText=`-${data.author}`;
 };
 getQuote();
 
-copyBtn.addEventListener("click",
-    function () {
-        navigator.clipboard.writeText(quoteText.innerText);
-        copyBtn.innerText="copied";
-    }
-)
+// tweet button
+
+    // tweetBtn.addEventListener("click",
+    //     function(){
+    //     let quote=quoteText.innerText;
+    //     let author=authorText.innerText;
+    //     let twitterUrl=`https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    //     window.open=(twitterUrl,"_Blank");
+    //     }
+    // );
