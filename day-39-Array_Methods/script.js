97eeef9345d1a2becd7ed8097499c2bd5b44dcb4
tirthpatel556 <pateltirth7654@gmail.com>
2026-03-25 23:05:56 +0530

    learned new array methods

---
+++ day-39-Array_Methods/script.js
@@ -0,0 +1,33 @@
+// let nums=[1,2,3,4,5];
+// let newNums=[...nums];
+// console.log(newNums);
+
+// let str="hello";
+// let newStr=str;
+// console.log(...newStr);
+
+// let student =[
+//     {
+//         name:"skirk",
+//         marks:98,        
+//     },
+//     {
+//         name:"tirth",
+//         marks:95
+//     },
+//     {
+//         name:"lauma",
+//         marks:90
+//     },
+//     {
+//         name:"kuki",
+//         marks:80
+//     },
+//     {
+//         name:"esco",
+//         marks:88
+//     }
+// ];
+// let [winner,runnerUp,secoundRunnerUp,...other]= student;
+// console.log(winner);
+// console.log(other);
