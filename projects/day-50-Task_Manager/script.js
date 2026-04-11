eca82e6250407796e2ef3f968aa22bac8c3eb000
tirthpatel556 <pateltirth7654@gmail.com>
2026-04-11 14:04:29 +0530

    done initial styling

--- projects/day-50-Task_Manager/script.js
+++ projects/day-50-Task_Manager/script.js
@@ -5,8 +5,9 @@ let taskList = document.getElementById("taskList");
 let taskCount = document.getElementById("taskCount");
 
 // data stores here in the array
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-let tasks = [];
+let currentFilter = "all";
 
 // add button logic
 
@@ -22,12 +23,14 @@ addButton.addEventListener("click",
         };
 
         tasks.push(newTask);
+        saveData();
         renderTasks();
         tasksInput.value="";
     }
 );
 
 // for getting input with enter key press
+
 tasksInput.addEventListener("keypress",
     (e)=>{
         if (e.key === "Enter") {
@@ -45,9 +48,79 @@ function renderTasks() {
     } else {
         emptyState.style.display = "none";
     }
-    tasks.forEach((task,index) => {
+// filter task logic
+
+    let filteredTasks = tasks;
+    if (currentFilter === "completed") {
+        filteredTasks = tasks.filter(task => task.completed);
+    } else if(currentFilter === "pending") {
+        filteredTasks = tasks.filter(task => !task.completed);
+    }
+    
+    // add task in list
+    
+    filteredTasks.forEach((task,index) => {
+        
+        let realIndex = tasks.indexOf(task);
         let li = document.createElement("li");
         li.innerText = task.text;
+        
+        if (task.completed) {
+            li.classList.add("completed");
+        }
+        
+        li.addEventListener("click",
+            ()=>{
+                task.completed = !task.completed;
+                saveData();
+                renderTasks();
+            }
+        );
+        
+        // delete task
+        
+        let DeleteBtn = document.createElement("button");
+        DeleteBtn.innerText = "Delete";
+        
+        DeleteBtn.addEventListener("click",
+            (e)=>{
+                e.stopPropagation();
+                
+                tasks.splice(realIndex, 1);
+                saveData();
+                renderTasks();
+            }
+        );
         taskList.appendChild(li);
+        li.appendChild(DeleteBtn);
     });
+    
+    // total and completed count
+    
+    let total = tasks.length;
+    let completed = tasks.filter(t => t.completed).length;
+    taskCount.innerText = `Total: ${total} | Completed: ${completed}`;
 }
+
+// filter logic
+
+let filters = document.querySelectorAll('input[name="filter"]');
+
+filters.forEach(filter=>{
+    filter.addEventListener("change",
+        ()=>{
+            currentFilter = filter.value;
+            renderTasks();
+        }
+    );
+});
+
+//save data after refresh
+
+function saveData(){
+    localStorage.setItem("tasks",JSON.stringify(tasks));
+}
+
+// initial call
+
+renderTasks();
