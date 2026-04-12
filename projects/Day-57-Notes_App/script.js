f51c0066e10f9f333bebb937c8ec1e09777bc1ca
tirthpatel556 <pateltirth7654@gmail.com>
2026-04-12 18:37:22 +0530

    done final changes

--- projects/Day-57-Notes_App/script.js
+++ projects/Day-57-Notes_App/script.js
@@ -2,35 +2,94 @@ let noteInput = document.getElementById("noteInput");
 let categoryInput = document.getElementById("categoryInput");
 let addBtn = document.getElementById("addNote");
 let notesContainer = document.getElementById("notesContainer");
+let searchInput = document.getElementById("searchInput");
 
 let notes = [];
+let editIndex = null;
 
-addBtn.addEventListener("click",
-    ()=>{
-        let text = noteInput.value;
-        let category = categoryInput.value;
-        if (text === "") {
-            return;
-        }
-        let newNote = {
-            text : text,
+// add note logic
+
+addBtn.addEventListener("click", () => {
+
+    let text = noteInput.value;
+    let category = categoryInput.value;
+    if (text === "") return;    //prevents blank input
+
+    // to add note
+
+    if (editIndex === null) {
+
+        notes.push({
+            text: text,
+            category: category,
+        });
+    } else {
+
+    //   to edit note
+
+        notes[editIndex]= {
+            text :text,
             category : category
-        }
-        notes.push(newNote);
-        renderNotes();
-        noteInput.value = "";
-    }
-);
+        };
+        editIndex = null;
+        addBtn.innerText = "Add Note";
+    };
+    renderNotes();
+    noteInput.value = "";
+});
 
-function renderNotes(){
-    noteInput.innerHTML = "";
+// render notes logic
 
-    notes.forEach((note,index) =>{
+function renderNotes() {
+  notesContainer.innerHTML = "";
+  
+  let searchText = searchInput.value.toLowerCase();
+
+  notes.forEach((note, index) => {
+    if (note.text.toLowerCase().includes(searchText)) {
+        
         let div = document.createElement("div");
         div.classList.add("note");
-        div.innerHTML =`<strong>${note.text}</strong><br>
+        let text = document.createElement("p");
+        text.innerHTML = `
+        <p>${note.text}</p>
         <small>${note.category}</small>
+        <button onclick="editNote(${index})">Edit</button>
+        <button onclick="deleteNote(${index})">Delete</button>
         `;
-        notesContainer.appendChild(div); 
-    });
+    }
+    
+    let editBtn = document.createElement("button");
+    editBtn.innerText = "Edit";
+    
+    editBtn.addEventListener("click",
+        ()=>{
+            noteInput.value = note.text;
+            categoryInput.value = note.category;
+
+            editIndex = index;
+            addBtn.innerText = "Update Note";
+            console.log("editIndex:" +editIndex);
+        }
+    );
+    div.appendChild(text);
+    div.appendChild(editBtn);
+    notesContainer.appendChild(div);
+});
 }
+
+// delete note logic
+
+function deleteNote(index) {
+    notes.splice(index,1);
+    localStorage.setItem("notes",JSON.stringify("notes"));
+    renderNotes();
+}
+
+// search note logic
+
+searchInput.addEventListener("input",
+    ()=>{
+        renderNotes();
+    }
+);
