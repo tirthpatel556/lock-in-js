4a1c8571bf62287cf95e6580e1e3f454772757ea
tirthpatel556 <pateltirth7654@gmail.com>
2026-04-15 21:56:08 +0530

    fixed ui

--- projects/Day-57-Notes_App/script.js
+++ projects/Day-57-Notes_App/script.js
@@ -5,7 +5,7 @@ let notesContainer = document.getElementById("notesContainer");
 let searchInput = document.getElementById("searchInput");
 
 let notes = [];
-let editIndex = null;
+let editId = null;
 
 // add note logic
 
@@ -14,81 +14,131 @@ addBtn.addEventListener("click", () => {
     let text = noteInput.value;
     let category = categoryInput.value;
     if (text === "") return;    //prevents blank input
-
+    
     // to add note
-
-    if (editIndex === null) {
+    
+    if (editId === null) {
 
         notes.push({
+            id: Date.now(),
             text: text,
-            category: category,
+            category: category
         });
     } else {
-
+        
     //   to edit note
 
-        notes[editIndex]= {
-            text :text,
-            category : category
-        };
-        editIndex = null;
-        addBtn.innerText = "Add Note";
-    };
-    renderNotes();
-    noteInput.value = "";
+    notes = notes.map(note =>{
+        if (note.id === editId) {
+            return {
+                ...note,
+                text : text,
+                category : category
+            };
+        }
+        return note;
+    });
+    editId = null;
+    addBtn.innerText = "Add Note";
+};
+renderNotes();
+noteInput.value = "";
 });
 
 // render notes logic
 
 function renderNotes() {
-  notesContainer.innerHTML = "";
-  
-  let searchText = searchInput.value.toLowerCase();
-
-  notes.forEach((note, index) => {
-    if (note.text.toLowerCase().includes(searchText)) {
-        
-        let div = document.createElement("div");
-        div.classList.add("note");
-        let text = document.createElement("p");
-        text.innerHTML = `
-        <p>${note.text}</p>
-        <small>${note.category}</small>
-        <button onclick="editNote(${index})">Edit</button>
-        <button onclick="deleteNote(${index})">Delete</button>
-        `;
-    }
     
-    let editBtn = document.createElement("button");
-    editBtn.innerText = "Edit";
-    
-    editBtn.addEventListener("click",
-        ()=>{
-            noteInput.value = note.text;
-            categoryInput.value = note.category;
+    let found = false;
+    notes.forEach((note)=>{
+        if (note.text.toLowerCase().includes(searchText)) {
+            found = true;
+            if (notes.length === 0) {
+                notesContainer.innerHTML = "<p>No notes yet!</p>";
+                return;
+            }
+            notesContainer.innerHTML = "";
+            
+            let searchText = searchInput.value.toLowerCase();
+            let selectedCategory = categoryInput.value;
+            
+            let filteredNotes = notes.filter(note=>{
+              let matchesSearch = note.text.toLowerCase().includes(searchText);
+              let matchesCategory = selectedCategory === "All" || note.category === selectedCategory;
+            
+              return matchesSearch && matchesCategory;
+            });
+            
+            if (filteredNotes.length === 0) {
+            notesContainer.innerHTML = "<p>No notes found</p>";
+            return;
+            }
+            
+            filteredNotes.forEach((note, index) => {
+            if (note.text.toLowerCase().includes(searchText)) {
+                
+                let div = document.createElement("div");
+                div.classList.add("note");
+                let text = document.createElement("p");
+                text.innerText = note.text;
+                let category = document.createElement("p");
+                category.innerText = note.category;
+            
+                let editBtn = document.createElement("button");
+                editBtn.innerText = "Edit";
+                
+                editBtn.addEventListener("click",
+                    ()=>{
+                        noteInput.value = note.text;
+                        categoryInput.value = note.category;
+            
+                        editId = note.id;
+                        addBtn.innerText = "Update Note";
+                    }
+                );
+                
+                // delete button
+                
+                let deleteBtn = document.createElement("button");
+                deleteBtn.innerText = "Delete";
+            
+                deleteBtn.addEventListener("click",
+                    ()=>{
+                        notes = notes.filter(n => n.id !== note.id);
+                        localStorage.setItem("notes",JSON.stringify("notes"));
+                        renderNotes();
+                    }
+                );
+                // to save
+                localStorage.setItem("notes",JSON.stringify(notes));
+                div.appendChild(text);
+                div.appendChild(category);
+                div.appendChild(editBtn);
+                div.appendChild(deleteBtn);
+                notesContainer.appendChild(div);
 
-            editIndex = index;
-            addBtn.innerText = "Update Note";
-            console.log("editIndex:" +editIndex);
         }
-    );
-    div.appendChild(text);
-    div.appendChild(editBtn);
-    notesContainer.appendChild(div);
-});
+    });
+
+}
+if(!found){
+    notesContainer.innerHTML = "<p>no matching notes found </p>";
 }
 
-// delete note logic
-
-function deleteNote(index) {
-    notes.splice(index,1);
-    localStorage.setItem("notes",JSON.stringify("notes"));
-    renderNotes();
+});
 }
 
 // search note logic
 
 searchInput.addEventListener("input",
+    ()=>{
+        renderNotes();
+    }
+);
+
+// filter category logic
+
+categoryInput.addEventListener("change",
     ()=>{
         renderNotes();
     }
